45059084d42c4bfbda756216ad51cc38340920c1
Tiago Camolesi <tiagocamolesi@gmail.com>
2011-09-24 17:28:25 -0300

    Fixed indentation.

--- src/assembler.c
+++ src/assembler.c
@@ -30,17 +30,17 @@
 #undef _IFUNC
 
 struct Assembler {
-    char *base, *pos, *data, *end;
-    int id;
-    AssemblerFactory origin;
+  char *base, *pos, *data, *end;
+  int id;
+  AssemblerFactory origin;
 };
 
 struct AssemblerFactory {
-    int maxProgramSize;
-    int maxProgramCount;
-    void *dataPool;
-    int *availableSlots;
-    int curSlot;
+  int maxProgramSize;
+  int maxProgramCount;
+  void *dataPool;
+  int *availableSlots;
+  int curSlot;
 };
 
 typedef void (*AsmFunc)(Assembler a, Element e);
@@ -71,56 +71,56 @@ void printCode(Assembler a);
 
 AssemblerFactory createAssemblerFactory(int maxProgramSize,
     int maxProgramCount) {
-        AssemblerFactory factory = malloc(sizeof(struct AssemblerFactory));
+  AssemblerFactory factory = malloc(sizeof(struct AssemblerFactory));
 
-        factory->curSlot = 0;
-        factory->maxProgramCount = maxProgramCount;
-        factory->maxProgramSize = maxProgramSize;
+  factory->curSlot = 0;
+  factory->maxProgramCount = maxProgramCount;
+  factory->maxProgramSize = maxProgramSize;
 
-        factory->availableSlots = calloc(sizeof(int), maxProgramCount);
-        factory->dataPool = mmap(NULL, maxProgramSize * maxProgramCount,
-         PROT_EXEC | PROT_READ | PROT_WRITE,MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+  factory->availableSlots = calloc(sizeof(int), maxProgramCount);
+  factory->dataPool = mmap(NULL, maxProgramSize * maxProgramCount,
+      PROT_EXEC | PROT_READ | PROT_WRITE,MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 
-        return factory;
+  return factory;
 }
 
 void destroyAssemblerFactory(AssemblerFactory factory) {
-    if( factory->availableSlots != 0 ) {
+  if( factory->availableSlots != 0 ) {
     free(factory->availableSlots);
-    }
+  }
 
-    if(factory->dataPool != 0) {
+  if(factory->dataPool != 0) {
     munmap(factory->dataPool, factory->maxProgramCount * factory->maxProgramSize );
-    }
+  }
 
-    free(factory);
+  free(factory);
 }
 
 void getAvailableSlot(AssemblerFactory factory, Assembler a) {
-    int i;
-    int ind = factory->curSlot;
+  int i;
+  int ind = factory->curSlot;
 
-    a->id = -1;
+  a->id = -1;
 
-    i = ind;
-    do {
-        if( ++i >= factory->maxProgramCount ) {
-            i = 0;
-        }
-        if( factory->availableSlots[i] == 0 ) {
-            factory->curSlot = i;
-            factory->availableSlots[i] = 1;
+  i = ind;
+  do {
+    if( ++i >= factory->maxProgramCount ) {
+      i = 0;
+    }
+    if( factory->availableSlots[i] == 0 ) {
+      factory->curSlot = i;
+      factory->availableSlots[i] = 1;
 
-            a->id = i;
-            a->base = (void*) ( factory->dataPool + i * factory->maxProgramSize);
+      a->id = i;
+      a->base = (void*) ( factory->dataPool + i * factory->maxProgramSize);
 
-            return;
-        }
-    } while( i != ind );
+      return;
+    }
+  } while( i != ind );
 }
 
 void freeSlot(AssemblerFactory factory, int id) {
-    factory->availableSlots[id] = 0;
+  factory->availableSlots[id] = 0;
 }
 
 //------------------------------------------------------------------------------
@@ -128,46 +128,46 @@ void freeSlot(AssemblerFactory factory, int id) {
 //------------------------------------------------------------------------------
 
 Assembler createAssembler(AssemblerFactory factory) {
-    Assembler a = malloc(sizeof(struct Assembler));
+  Assembler a = malloc(sizeof(struct Assembler));
 
-    getAvailableSlot(factory, a);
+  getAvailableSlot(factory, a);
 
-    if( a->id == -1 ) {
-        free(a);
-        return 0;
-    }
+  if( a->id == -1 ) {
+    free(a);
+    return 0;
+  }
 
-    a->origin = factory;
-    a->end = a->base + a->origin->maxProgramSize - 1;
-    a->data = a->end;
-    a->pos = a->base;
+  a->origin = factory;
+  a->end = a->base + a->origin->maxProgramSize - 1;
+  a->data = a->end;
+  a->pos = a->base;
 
-    int i = 0;
-    for( i = 0; i < a->origin->maxProgramSize; i++ ) {
-        a->base[i] = 0;
-    }
+  int i = 0;
+  for( i = 0; i < a->origin->maxProgramSize; i++ ) {
+    a->base[i] = 0;
+  }
 
-    return a;
+  return a;
 }
 
 void destroyAssembler(Assembler a) {
-    freeSlot(a->origin, a->id);
-    free(a);
+  freeSlot(a->origin, a->id);
+  free(a);
 }
 
 JITFunction getJitFunction(Assembler a) {
-    return (JITFunction) a->base;
+  return (JITFunction) a->base;
 }
 
 void assembleTree(Assembler a, ETTree t) {
-    int i;
-    int bc = getBranchCount(t);
-    if( bc > 0 ) {
-        for(i = 0; i < bc; i++ ) {
-            assembleTree(a, getBranch(t, i));
-        }
+  int i;
+  int bc = getBranchCount(t);
+  if( bc > 0 ) {
+    for(i = 0; i < bc; i++ ) {
+      assembleTree(a, getBranch(t, i));
     }
-    assembleElement(a, getElement(t));
+  }
+  assembleElement(a, getElement(t));
 }
 
 //------------------------------------------------------------------------------
@@ -175,15 +175,15 @@ void assembleTree(Assembler a, ETTree t) {
 //------------------------------------------------------------------------------
 
 void assembleElement(Assembler a, Element e) {
-    _asmFuncs[e.type](a, e);
+  _asmFuncs[e.type](a, e);
 }
 
 void assemble_operator(Assembler a, Element e) {
-    _asmOps[e.op](a,e);
+  _asmOps[e.op](a,e);
 }
 
 void assemble_ifunc(Assembler a, Element e) {
-    _asmIFuncs[e.func](a,e);
+  _asmIFuncs[e.func](a,e);
 }
 
 //------------------------------------------------------------------------------
@@ -191,32 +191,32 @@ void assemble_ifunc(Assembler a, Element e) {
 //------------------------------------------------------------------------------
 
 inline void insertCode(Assembler a, void *code, int size ) {
-    memcpy(a->pos, code, size);
-    a->pos += size;
+  memcpy(a->pos, code, size);
+  a->pos += size;
 }
 
 inline void insertCodeByte(Assembler a, char code) {
-    *(a->pos) = code;
-    a->pos++;
+  *(a->pos) = code;
+  a->pos++;
 }
 
 inline char* insertData(Assembler a, void *code, int size ) {
-    a->data -= size;
-    memcpy(a->data, code, size);
+  a->data -= size;
+  memcpy(a->data, code, size);
 
-    return a->data;
+  return a->data;
 }
 
 void printCode(Assembler a) {
-    int i;
-    for(i = 0; i < a->origin->maxProgramSize; i++ ) {
-        if( i > 0 && i % 16 == 0 ) {
-            printf( "\n" );
-        }
-        printf("%.2x ", ((int) a->base[i]) & 0xFF );
+  int i;
+  for(i = 0; i < a->origin->maxProgramSize; i++ ) {
+    if( i > 0 && i % 16 == 0 ) {
+      printf( "\n" );
     }
+    printf("%.2x ", ((int) a->base[i]) & 0xFF );
+  }
 
-    printf("\n-----------------------------------\n");
+  printf("\n-----------------------------------\n");
 }
 
 //------------------------------------------------------------------------------
@@ -224,34 +224,34 @@ void printCode(Assembler a) {
 //------------------------------------------------------------------------------
 
 void beginAssembly(Assembler a) {
-    a->pos = a->base;
-    a->data = a->end;
+  a->pos = a->base;
+  a->data = a->end;
 
-    //#PUSH EBP
-    //#MOV EBP, ESP
-    //PUSH EBX
-    //PUSH EDI
-    //MOV EBX, a->data
-    //#MOV EDI, DWORD PTR [EBP + 0x08]
-    //MOV EDI, DWORD PTR [ESP + 0x0C]
+  //#PUSH EBP
+  //#MOV EBP, ESP
+  //PUSH EBX
+  //PUSH EDI
+  //MOV EBX, a->data
+  //#MOV EDI, DWORD PTR [EBP + 0x08]
+  //MOV EDI, DWORD PTR [ESP + 0x0C]
 
-    /*insertCode(a, "\x55\x89\xE5\x53\x57\xBB", 6);
+  /*insertCode(a, "\x55\x89\xE5\x53\x57\xBB", 6);
     insertCode(a, &a->data, 4 );
     insertCode(a, "\x8B\x7D\x08", 3);*/
 
-    insertCode(a, "\x53\x57\xBB", 3);
-    insertCode(a, &a->data, 4 );
-    insertCode(a, "\x8B\x7C\x24\x0C", 4);
+  insertCode(a, "\x53\x57\xBB", 3);
+  insertCode(a, &a->data, 4 );
+  insertCode(a, "\x8B\x7C\x24\x0C", 4);
 }
 
 void finishAssembly(Assembler a) {
-    //POP EDI
-    //POP EBX
-    //#LEAVE
-    //RET
+  //POP EDI
+  //POP EBX
+  //#LEAVE
+  //RET
 
-    //insertCode(a, "\x5F\x5B\xC9\xC3", 4 );
-    insertCode(a, "\x5F\x5B\xC3", 3 );
+  //insertCode(a, "\x5F\x5B\xC9\xC3", 4 );
+  insertCode(a, "\x5F\x5B\xC3", 3 );
 }
 
 //------------------------------------------------------------------------------
@@ -259,87 +259,87 @@ void finishAssembly(Assembler a) {
 //------------------------------------------------------------------------------
 
 void assemble_variable(Assembler a, Element e) {
-    //FLD DWORD PTR [EDI + varnum * 4]
+  //FLD DWORD PTR [EDI + varnum * 4]
 
-    if( e.varnum < 32 ) { //Byte displacement
-        insertCode(a, "\xD9\x47", 2);
-        insertCodeByte(a, (e.varnum * 4) & 0xFF );
-    } else { //Dword displacement
-        int tmp = e.varnum * 4;
-        insertCode(a, "\xD9\x87", 2);
-        insertCode(a, &tmp, 4 );
-    }
+  if( e.varnum < 32 ) { //Byte displacement
+    insertCode(a, "\xD9\x47", 2);
+    insertCodeByte(a, (e.varnum * 4) & 0xFF );
+  } else { //Dword displacement
+    int tmp = e.varnum * 4;
+    insertCode(a, "\xD9\x87", 2);
+    insertCode(a, &tmp, 4 );
+  }
 }
 
 void assemble_constant(Assembler a, Element e) {
-    //FLD dword [EBX + delta]
+  //FLD dword [EBX + delta]
 
-    char *ptr = insertData(a, &e.f, 4 );
-    int delta = ( (int) ptr ) - ((int) a->end );
+  char *ptr = insertData(a, &e.f, 4 );
+  int delta = ( (int) ptr ) - ((int) a->end );
 
-    if ( delta > -127 ) { //Byte displacement
-        insertCode(a, "\xD9\x43", 2);
-        insertCodeByte(a, delta & 0xFF );
-    } else { //Dword displacement
-        insertCode(a, "\xD9\x83", 2);
-        insertCode(a, &delta, 4 );
-    }
+  if ( delta > -127 ) { //Byte displacement
+    insertCode(a, "\xD9\x43", 2);
+    insertCodeByte(a, delta & 0xFF );
+  } else { //Dword displacement
+    insertCode(a, "\xD9\x83", 2);
+    insertCode(a, &delta, 4 );
+  }
 }
 
 void assemble_op_add(Assembler a, Element e) {
-    //ADDP st1
+  //ADDP st1
 
-    insertCode(a, "\xDE\xC1", 2);
+  insertCode(a, "\xDE\xC1", 2);
 }
 
 void assemble_op_mul(Assembler a, Element e) {
-    //MULP st1
+  //MULP st1
 
-    insertCode(a, "\xDE\xC9", 2);
+  insertCode(a, "\xDE\xC9", 2);
 }
 
 void assemble_op_div(Assembler a, Element e) {
-    //DIVP st1
+  //DIVP st1
 
-    insertCode(a, "\xDE\xF9", 2);
+  insertCode(a, "\xDE\xF9", 2);
 }
 
 void assemble_op_sub(Assembler a, Element e) {
-    //SUBP st1
+  //SUBP st1
 
-    insertCode(a, "\xDE\xE9", 2);
+  insertCode(a, "\xDE\xE9", 2);
 }
 
 void assemble_ifunc_max(Assembler a, Element e) {
-    //FUCOMI st1
-    //FCMOVB st1
-    //FFREE st1
+  //FUCOMI st1
+  //FCMOVB st1
+  //FFREE st1
 
-    insertCode(a, "\xDB\xE9\xDA\xC1\xDD\xD9", 6);
+  insertCode(a, "\xDB\xE9\xDA\xC1\xDD\xD9", 6);
 }
 
 void assemble_ifunc_min(Assembler a, Element e) {
-    //FUCOMI st1
-    //FCMOVNB st1
-    //FSTP st1
+  //FUCOMI st1
+  //FCMOVNB st1
+  //FSTP st1
 
-    insertCode(a, "\xDB\xE9\xDB\xC1\xDD\xD9", 6);
+  insertCode(a, "\xDB\xE9\xDB\xC1\xDD\xD9", 6);
 }
 
 void assemble_ifunc_sin(Assembler a, Element e) {
-    //FSIN
+  //FSIN
 
-    insertCode(a, "\xD9\xFE", 2);
+  insertCode(a, "\xD9\xFE", 2);
 }
 
 void assemble_ifunc_cos(Assembler a, Element e) {
-    //FCOS
+  //FCOS
 
-    insertCode(a, "\xD9\xFF", 2);
+  insertCode(a, "\xD9\xFF", 2);
 }
 
 void assemble_ifunc_abs(Assembler a, Element e) {
-    //FABS
+  //FABS
 
-    insertCode(a, "\xD9\xE1", 2);
+  insertCode(a, "\xD9\xE1", 2);
 }
--- src/element.c
+++ src/element.c
@@ -7,14 +7,14 @@
 //                                Headers
 //-----------------------------------------------------------------------------
 
-//-----------------------------------------------------------------------------
-//                             Implementation
-//-----------------------------------------------------------------------------
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <memory.h>
 #include "element.h"
+
+//-----------------------------------------------------------------------------
+//                             Implementation
+//-----------------------------------------------------------------------------
 
 #define _OP(x,y,z) z,
 char *_opstring[] = {
@@ -29,140 +29,140 @@ IFuncInfo _ifuncs[] = {
 #undef _IFUNC
 
 RandomElementParams _randomizerParams = {
-    { 400, 150, 350, 100},
-    { 300, 250, 300, 100 },
-    { 300, 300, 250, 75, 75 },
-    { -1000.0f, 1000.0f },
-    { 0, 2 }
+  { 400, 150, 350, 100},
+  { 300, 250, 300, 100 },
+  { 300, 300, 250, 75, 75 },
+  { -1000.0f, 1000.0f },
+  { 0, 2 }
 };
 
 void setRandomElementParams( const RandomElementParams* params ) {
-    memcpy( &_randomizerParams, params, sizeof( RandomElementParams ) );
+  memcpy( &_randomizerParams, params, sizeof( RandomElementParams ) );
 }
 
 void getRandomElementParams( RandomElementParams* params ) {
-    memcpy( params, &_randomizerParams, sizeof( RandomElementParams ) );
+  memcpy( params, &_randomizerParams, sizeof( RandomElementParams ) );
 }
 
 Element getRandomElement2(Element base) {
-    Element e;
+  Element e;
 
-    e.type = base.type;
-    int i, tmp;
-    if( base.type == ELEM_OPERATOR ) {
-            e.op = 0;
-            tmp = rand() % 1000;
-            for ( i = 0; i < OPTYPE_MAX; i++ ) {
-                    tmp -= _randomizerParams.probOperators[i];
-                    if ( tmp <= 0 ) {
-                            e.op = i;
-                            break;
-                        }
-                }
-    } else if( base.type == ELEM_CONSTANT || base.type == ELEM_VARIABLE ) {
-                if( rand() % 2 == 0 ) {
-                    e.type = ELEM_CONSTANT;
-                    float cr0 = _randomizerParams.constantRange[0];
-                    float cr1 = _randomizerParams.constantRange[1];
-                    e.f = rand() / ( 1.0f * RAND_MAX ) * ( cr1 - cr0 ) + cr0;
-                    } else {
-                        e.type = ELEM_VARIABLE;
-                        int v0 = _randomizerParams.variablesRange[0];
-                        int v1 = _randomizerParams.variablesRange[1];
-                        e.varnum = rand() % ( v1 - v0 ) + v0;
-                    }
-    } else if( base.type == ELEM_IFUNC ) {
-            e.func = base.func;
+  e.type = base.type;
+  int i, tmp;
+  if( base.type == ELEM_OPERATOR ) {
+    e.op = 0;
+    tmp = rand() % 1000;
+    for ( i = 0; i < OPTYPE_MAX; i++ ) {
+      tmp -= _randomizerParams.probOperators[i];
+      if ( tmp <= 0 ) {
+        e.op = i;
+        break;
+      }
     }
+  } else if( base.type == ELEM_CONSTANT || base.type == ELEM_VARIABLE ) {
+    if( rand() % 2 == 0 ) {
+      e.type = ELEM_CONSTANT;
+      float cr0 = _randomizerParams.constantRange[0];
+      float cr1 = _randomizerParams.constantRange[1];
+      e.f = rand() / ( 1.0f * RAND_MAX ) * ( cr1 - cr0 ) + cr0;
+    } else {
+      e.type = ELEM_VARIABLE;
+      int v0 = _randomizerParams.variablesRange[0];
+      int v1 = _randomizerParams.variablesRange[1];
+      e.varnum = rand() % ( v1 - v0 ) + v0;
+    }
+  } else if( base.type == ELEM_IFUNC ) {
+    e.func = base.func;
+  }
 
-    return e;
+  return e;
 }
 
 Element getRandomElement() {
-    Element e;
-    int i;
+  Element e;
+  int i;
 
-    e.type = 0;
-    int tmp = rand() % 1000;
-    for ( i = 0; i < ELEMTYPE_MAX; i++ ) {
-            tmp -= _randomizerParams.probType[i];
-            if ( tmp <= 0 ) {
-                    e.type = i;
-                    break;
-                }
+  e.type = 0;
+  int tmp = rand() % 1000;
+  for ( i = 0; i < ELEMTYPE_MAX; i++ ) {
+    tmp -= _randomizerParams.probType[i];
+    if ( tmp <= 0 ) {
+      e.type = i;
+      break;
+    }
+  }
+
+  switch ( e.type ) {
+    case ELEM_OPERATOR:
+      e.op = 0;
+      tmp = rand() % 1000;
+      for ( i = 0; i < OPTYPE_MAX; i++ ) {
+        tmp -= _randomizerParams.probOperators[i];
+        if ( tmp <= 0 ) {
+          e.op = i;
+          break;
         }
+      }
+      break;
 
-    switch ( e.type ) {
-        case ELEM_OPERATOR:
-            e.op = 0;
-            tmp = rand() % 1000;
-            for ( i = 0; i < OPTYPE_MAX; i++ ) {
-                    tmp -= _randomizerParams.probOperators[i];
-                    if ( tmp <= 0 ) {
-                            e.op = i;
-                            break;
-                        }
-                }
-            break;
+    case ELEM_CONSTANT:
+      {
+        float cr0 = _randomizerParams.constantRange[0];
+        float cr1 = _randomizerParams.constantRange[1];
+        e.f = rand() / ( 1.0f * RAND_MAX ) * ( cr1 - cr0 ) + cr0;
+      }
+      break;
 
-        case ELEM_CONSTANT:
-            {
-                float cr0 = _randomizerParams.constantRange[0];
-                float cr1 = _randomizerParams.constantRange[1];
-                e.f = rand() / ( 1.0f * RAND_MAX ) * ( cr1 - cr0 ) + cr0;
-            }
-            break;
+    case ELEM_VARIABLE:
+      {
+        int v0 = _randomizerParams.variablesRange[0];
+        int v1 = _randomizerParams.variablesRange[1];
+        e.varnum = rand() % ( v1 - v0 ) + v0;
+      }
+      break;
 
-        case ELEM_VARIABLE:
-            {
-                int v0 = _randomizerParams.variablesRange[0];
-                int v1 = _randomizerParams.variablesRange[1];
-                e.varnum = rand() % ( v1 - v0 ) + v0;
-            }
-            break;
-
-        case ELEM_IFUNC:
-            e.func = 0;
-            tmp = rand() % 1000;
-            for ( i = 0; i < IFUNCTYPE_MAX; i++ ) {
-                    tmp -= _randomizerParams.probIFunc[i];
-                    if ( tmp <= 0 ) {
-                            e.func = i;
-                            break;
-                        }
-                }
-            break;
-
-        default:
-            break;
+    case ELEM_IFUNC:
+      e.func = 0;
+      tmp = rand() % 1000;
+      for ( i = 0; i < IFUNCTYPE_MAX; i++ ) {
+        tmp -= _randomizerParams.probIFunc[i];
+        if ( tmp <= 0 ) {
+          e.func = i;
+          break;
         }
+      }
+      break;
 
-    return e;
+    default:
+      break;
+  }
+
+  return e;
 }
 
 void printElement( Element e ) {
-    switch ( e.type ) {
-        case ELEM_OPERATOR:
-            printf( "%s", _opstring[e.op] );
-            break;
+  switch ( e.type ) {
+    case ELEM_OPERATOR:
+      printf( "%s", _opstring[e.op] );
+      break;
 
-        case ELEM_VARIABLE:
-            printf( "$%d", e.varnum );
-            break;
+    case ELEM_VARIABLE:
+      printf( "$%d", e.varnum );
+      break;
 
-        case ELEM_CONSTANT:
-            printf( "%.2f", e.f );
-            break;
+    case ELEM_CONSTANT:
+      printf( "%.2f", e.f );
+      break;
 
-        case ELEM_IFUNC:
-            printf( "%s", _ifuncs[e.func].name );
-            break;
+    case ELEM_IFUNC:
+      printf( "%s", _ifuncs[e.func].name );
+      break;
 
-        default:
-            break;
-        }
+    default:
+      break;
+  }
 }
 
 IFuncInfo getIFuncInfo(int id) {
-    return _ifuncs[id];
+  return _ifuncs[id];
 }
